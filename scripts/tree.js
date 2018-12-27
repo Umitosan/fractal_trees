c8cac62fade5f9bd86e808ea6250ddb78fe393ac
Umitosan <yeahdom@gmail.com>
2018-12-27 01:41:26 -0800

    balance branches

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,19 +1,19 @@
 /*jshint esversion: 6 */
 
-var sizeRatio = 0.6;
+var sizeRatio = 0.7;
 var treeAngle = 10;
-var treeAngleVel = 0.1;
+var treeAngleVel = 0.01;
 
 function resetTreeExtras() {
-  sizeRatio = 0.6;
+  sizeRatio = 0.7;
   treeAngle = 10;
-  treeAngleVel = 0.1;
+  treeAngleVel = 0.01;
 }
 
 function Tree(x,y) {
   this.seedX = x;
   this.seedY = y;
-  this.size = 300; // pixel size of main trunk
+  this.size = 200; // pixel size of main trunk
   this.sizeRatio = 0.6;
   this.treeAngle = 10;
   this.reeAngleVel = 0.1;
@@ -29,7 +29,7 @@ function Tree(x,y) {
     ctx.translate(this.seedX,this.seedY);
     ctx.beginPath();
     ctx.strokeStyle = myColors.green;
-    ctx.lineWidth = 4;
+    ctx.lineWidth = 2;
     ctx.moveTo(0,0);
     ctx.lineTo(0,0-this.size);
     ctx.stroke();
@@ -58,20 +58,20 @@ function drawBranch(startX,startY,length,angleFromCenter) {
   ctx.translate(startX,startY);
   ctx.rotate(  getRadianAngle(angleFromCenter) );
   ctx.beginPath();
-  ctx.strokeStyle = myColors.green;
-  ctx.lineWidth = 2;
+  ctx.strokeStyle = myColors.black;
+  ctx.lineWidth = 1;
   ctx.moveTo(0,0);
   ctx.lineTo(0,0-length);
   ctx.stroke();
   ctx.restore();
 
-  ctx.beginPath();
-  ctx.fillStyle = myColors.red;
-  ctx.fillRect(startX-1,startY-1,3,3);
+  // ctx.beginPath();
+  // ctx.fillStyle = myColors.red;
+  // ctx.fillRect(startX-1,startY-1,3,3);
 
   let newX = length * Math.cos(getRadianAngle(90-angleFromCenter));
   let newY = length * Math.sin(getRadianAngle(90-angleFromCenter));
-  if (length > 10) { // drawBranch(startX,startY,length,angleFromCenter)
+  if (length > 4) { // drawBranch(startX,startY,length,angleFromCenter)
     drawBranch(startX+newX,startY-newY,length*sizeRatio,angleFromCenter+treeAngle);
     drawBranch(startX+newX,startY-newY,length*sizeRatio,angleFromCenter-treeAngle);
   } else {
